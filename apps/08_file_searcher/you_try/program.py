df4515f9468eafb35d85c944959cc3739f681cbb
infobots <44482010+infobots@users.noreply.github.com>
2020-01-13 16:06:42 -0800

    01

---
+++ apps/08_file_searcher/you_try/program.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  5 00:41:36 2020
+
+@author: datar
+"""
+
+def main():
+    pass
+
+def print_header():
+    pass
+
+def get_folder_from_user():
+    pass
+
+def get_search_text_from_user():
+    pass
+
+if__name__ == '__main__':
+    main()
---
+++ apps/untitled0.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  5 00:41:36 2020
+
+@author: datar
+"""
+
